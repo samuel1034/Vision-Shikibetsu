5f4f1fbd4d746a5c182fbef2bdc3db9b1985e7f5
Samuel Fuentes (Genius) <samuelgenius1034@gmail.com>
2025-05-09 17:37:32 -0400

    Finished app

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ This demo shows how to:
 - Deploy an AI application
 """)
 st.sidebar.write("[Github Repository] (https://github.com/samuel1034/Vision-Shikibetsu)")
+st.sidebar.write("Made with love ❤️ by Samuel Fuentes")
 
 #File Uploader
 uploaded_file = st.file_uploader(
@@ -50,13 +51,13 @@ uploaded_file = st.file_uploader(
 if uploaded_file is not None:
     #Display the upload image
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     #Classify the image
     with st.spinner("Classifying Image"):
         try:
             #Preprocess the image
-            inputs = processor(image=image, return_tensors="pt")
+            inputs = processor(images=image, return_tensors="pt")
 
             #Get predictions
             with torch.no_grad():
@@ -86,7 +87,20 @@ if uploaded_file is not None:
         except Exception as e:
             st.error(f"Error processing image: {str(e)}")
 
-        #
+#Add some examples
+st.subheader("Examples Images to Try")
+col1,col2, col3 = st.columns(3)
+with col1:
+    st.image("https://www.thesprucepets.com/thmb/ajN-cwmHvNjnAZW4RsIfc8yDFl8=/3600x0/filters:no_upscale():strip_icc()/dog-breeds-with-blue-eyes-5089039-01-27c7ad9283154896a8b59ecdbc911fcc.jpg",
+             caption="Example Dog", width=200)
+with col2:
+    st.image("https://th.bing.com/th/id/OIP.i-HIGbfQ6cQIt5a7RipD4wHaHa?cb=iwp1&rs=1&pid=ImgDetMain",
+             caption="Example: Cat", width=200)
+
+with col3:
+    st.image("https://upload.wikimedia.org/wikipedia/commons/9/90/Labrador_Retriever_portrait.jpg",
+             caption="Example: Labrador Retriever", width=200)
+
 
 
 
