3bf098ecae3c36cc07ccd0bfce7a9ec8dc904a5d
Samuel Fuentes (Genius) <samuelgenius1034@gmail.com>
2025-05-09 14:38:32 -0400

    Added file uploader

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image
-from transformers import VitImageProcessor, VitForImageClassification
+from transformers import ViTImageProcessor, ViTForImageClassification
 import torch
 
 # Set page config
@@ -13,17 +13,49 @@ st.set_page_config (
 #Load the pre-trained model and processor
 @st.cache_resource
 def load_model():
-    processor = VitImageProcessor.from_pretrained('google/vit-base-patch16-224')
-    model = VitForImageClassification.from_pretrained('google/vit-base-patch16-224')
+    processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
+    model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
     return processor, model
 
 processor, model = load_model()
 
 #Define class labels  (Using ImageNet Classes)
-with open ('iamagenet_classes.txt') as f:
+with open ('imagenet_classes.txt') as f:
     class_labels = [line.strip() for line in f.readlines()]
 
 # App title and description
+st.title("🖼️ Image Classification App")
+st.write ("""
+Upload an image, and this app will classify it into one of 1000 ImageNet categories.
+The app uses a Vision Transformer (ViT) model from Hugging Face Transformers.
+""")
+
+# Sidebar
+st.sidebar.header("About")
+st.sidebar.write("""
+This demo shows how to:
+- Use pre-trained vision models from Hugging Face
+- Create an interactive web app with Streamlit
+- Deploy an AI application
+""")
+st.sidebar.write("[Github Repository] (https://github.com/samuel1034/Vision-Shikibetsu)")
+
+#File Uploader
+
+uploaded_file = st.file.uploader(
+    "Choose an Image ...",
+    type=["jpg","jpeg", "png"],
+    help="Upload an image file (jpeg,png)"
+)
+
+if uploaded_file is not None:
+    #Display the upload image
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+
+
+
 
 
 
